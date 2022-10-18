68164fd69fbc8a6d4e70ac695b4b9edaf0981835
jj <jeyavinoth@gmail.com>
2022-10-18 13:41:53 -0400

    changed bindings for harpoon direct jump to files

--- init.lua
+++ init.lua
@@ -248,13 +248,15 @@ require("nvim-tree").setup()
 
 
 -- Harpoon mappings
-map("n", "<Leader>ha", ':lua require("harpoon.mark").add_file()<CR>', { noremap = true })
+map("n", "<Leader>a", ':lua require("harpoon.mark").add_file()<CR>', { noremap = true })
 map("n", "<Leader>l", ':lua require("harpoon.ui").toggle_quick_menu()<CR>', { noremap = true })
 map("n", "<Leader>hn", ':lua require("harpoon.ui").nav_next()<CR>', { noremap = true })
 map("n", "<Leader>hp", ':lua require("harpoon.ui").nav_prev()<CR>', { noremap = true })
-map("n", "<Leader>hf", ':lua require("harpoon.ui").nav_file(1)<CR>', { noremap = true })
-map("n", "<Leader>hd", ':lua require("harpoon.ui").nav_file(2)<CR>', { noremap = true })
-map("n", "<Leader>hs", ':lua require("harpoon.ui").nav_file(3)<CR>', { noremap = true })
+map("n", "<Leader>1", ':lua require("harpoon.ui").nav_file(1)<CR>', { noremap = true })
+map("n", "<Leader>2", ':lua require("harpoon.ui").nav_file(2)<CR>', { noremap = true })
+map("n", "<Leader>3", ':lua require("harpoon.ui").nav_file(3)<CR>', { noremap = true })
+map("n", "<Leader>4", ':lua require("harpoon.ui").nav_file(4)<CR>', { noremap = true })
+map("n", "<Leader>5", ':lua require("harpoon.ui").nav_file(5)<CR>', { noremap = true })
 
 
 -- Mappings.
