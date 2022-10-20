e8febdd7823199e0251f7983878a28277f802ffe
jj <jeyavinoth@gmail.com>
2022-10-20 14:33:06 -0400

    moved theme specific edits to hightlight.lua; changed to gruvbox

--- nvim/lua/highlights.lua
+++ nvim/lua/highlights.lua
@@ -8,3 +8,47 @@ vim.opt.pumblend = 5
 -- Background settings
 vim.opt.background = 'dark'
 -- vim.cmd([[highlight Normal ctermbg=None]])
+--
+
+-- Colorscheme settings
+
+-- NeoSolarized
+require('lualine').setup {
+    options = {
+        theme = 'solarized_dark'
+    }
+}
+local bufferline_status, bufferline = pcall(require, 'bufferline')
+if (bufferline_status) then
+    -- Neosolarized setup
+    bufferline.setup {
+        highlights = {
+            separator = {
+                fg = '#073642',
+                bg = '#002b36'
+            },
+            separator_selected = {
+                fg = '#073642',
+            },
+            background = {
+                fg = '#657b83',
+                bg = '#002b36'
+            },
+            buffer_selected = {
+                fg = '#fdf6e3',
+                bold = true
+            },
+            fill = {
+                bg = '#073642'
+            }
+        }
+    }
+end
+
+
+-- -- Gruvbox
+-- require('lualine').setup {
+--     options = {
+--         theme = 'gruvbox'
+--     }
+-- }
