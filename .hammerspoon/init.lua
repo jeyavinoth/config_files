bf1f76ac471fbb4c4ac426702afafb47054c8c49
jj <jeyavinoth@gmail.com>
2022-10-22 13:22:36 -0400

    show notification when I reload hammerspoon config

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,5 +1,8 @@
 -- Reload hammersppoon
-hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "r", function() hs.reload() end)
+hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "r", function()
+    hs.reload()
+    hs.notify.new({ title = "Hammerspoon", informativeText = "Config loaded" }):send()
+end)
 
 -- Keybinds
 require('lua/keybinds/devices') -- Device Binds
