05efe811997faa5a2c7ecf3bfbdceb493367d582
jj <jeyavinoth@gmail.com>
2022-10-31 15:24:00 -0400

    fixed first_run.sh

--- first_run.sh
+++ first_run.sh
@@ -2,15 +2,14 @@
 
 # Install vundle first for the vim files 
 # git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle 
-git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+# git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
 # linking all files to the home folder 
-ln -s ./.gitconfig ~/.gitconfig
-ln -s ./.gitignore ~/.gitignore
-ln -s ./.vim ~/.vim
-ln -s ./.tmux.conf ~/.tmux.conf
-ln -s ./.vim/nvim ~/.config/nvim
-ln -s ./.vim/.hammerspoon ~/.hammerspoon
+ln -s ${PWD}/.gitconfig ~/.gitconfig
+ln -s ${PWD}/.gitignore ~/.gitignore
+ln -s ${PWD}/.tmux.conf ~/.tmux.conf
+ln -s ${PWD}/nvim ~/.config/nvim
+ln -s ${PWD}/.hammerspoon ~/.hammerspoon
 
 # # making directories needed for my vim files
 # mkdir ~/.backup
@@ -22,5 +21,7 @@ ln -s ./.vim/.hammerspoon ~/.hammerspoon
 # echo 'alias rm="rm -i"' >> ~/.zshrc
 # echo 'alias ca="conda activate"' >> ~/.zshrc
 # echo 'source ~/.vim/aliases.sh' >> ~/.zshrc
-grep -qxF 'source ~/.vim/zsh_append.sh' ~/.zshrc || echo 'source ~/.vim/zsh_append.sh' >> ~/.zshrc
+
+zsh_append=${PWD}/zsh_append.sh
+grep -qxF "source ${zsh_append}" ~/.zshrc || echo "source ${zsh_append}" >> ~/.zshrc
 
