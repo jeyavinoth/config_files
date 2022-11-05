b88357322cfdb0f7b6a6364c8bf9beadd94a0660
jj <jeyavinoth@gmail.com>
2022-11-05 17:26:28 -0400

    added tmux shortcuts to zsh

--- zsh_append.sh
+++ zsh_append.sh
@@ -16,8 +16,12 @@ alias gdt="git difftool"
 alias gdtc="git difftool --cached"
 alias gwt="git worktree"
 
-# Tmux session
+# Tmux session / keybinds
 alias o="sh ~/.dotfiles/custom_scripts/tmux-session.sh"
+alias ta="tmux a -t"
+alias tn="tmux new -s"
+alias tls="tmux ls"
 
 # Bind key 
 bindkey -v
+
