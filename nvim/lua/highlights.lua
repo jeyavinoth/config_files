2c9eb9217ca2aa74db1dec1dc8cd34eb522fcc75
jj <jeyavinoth@gmail.com>
2022-11-05 18:47:34 -0400

    removed the hi DiffAdd

--- nvim/lua/highlights.lua
+++ nvim/lua/highlights.lua
@@ -7,8 +7,3 @@ vim.opt.pumblend = 5
 
 -- Background settings
 vim.opt.background = 'dark'
-
--- vim.cmd([[hi DiffAdd ctermfg=235 ctermbg=108]])
--- vim.cmd([[hi DiffAdd cterm=reverse ctermfg=108 ctermbg=235]])
--- vim.cmd([[hi Error cterm=reverse ctermfg=131]])
-
