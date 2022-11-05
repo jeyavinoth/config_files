7ee3aa9764ae4d192c5952b329bde5830e5352e0
jj <jeyavinoth@gmail.com>
2022-11-05 18:34:01 -0400

    commented out hi DiffAdd...

--- nvim/lua/highlights.lua
+++ nvim/lua/highlights.lua
@@ -7,3 +7,8 @@ vim.opt.pumblend = 5
 
 -- Background settings
 vim.opt.background = 'dark'
+
+-- vim.cmd([[hi DiffAdd ctermfg=235 ctermbg=108]])
+-- vim.cmd([[hi DiffAdd cterm=reverse ctermfg=108 ctermbg=235]])
+-- vim.cmd([[hi Error cterm=reverse ctermfg=131]])
+
