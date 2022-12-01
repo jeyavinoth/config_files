1fe2c7c886ce3798be165d45b84cd9fcfc75ee55
jj <jeyavinoth@watttime.org>
2022-12-01 12:18:51 -0500

    small change to highlights; selecting colorscheme

--- nvim/lua/highlights.lua
+++ nvim/lua/highlights.lua
@@ -11,13 +11,16 @@ vim.opt.background = 'dark'
 vim.cmd([[ au ColorScheme * hi Normal ctermbg=none guibg=none ]])
 
 -- Colorschemes
--- vim.cmd([[ colorscheme nightfox ]])
+vim.cmd([[ colorscheme duskfox ]])
 
--- Ayu-dark theme
-vim.cmd([[
-    set termguicolors     " enable true colors support
-    let ayucolor="dark"   " for dark version of theme
-    colorscheme ayu
-]])
--- let ayucolor="light"  " for light version of theme
--- let ayucolor="mirage" " for mirage version of theme
+-- -- Ayu-dark theme
+-- vim.cmd([[
+--     set termguicolors     " enable true colors support
+--     let ayucolor="dark"   " for dark version of theme
+--     colorscheme ayu
+-- ]])
+-- -- let ayucolor="light"  " for light version of theme
+-- -- let ayucolor="mirage" " for mirage version of theme
+
+-- -- Colorschemes
+-- vim.cmd([[ colorscheme gruvbox ]])
